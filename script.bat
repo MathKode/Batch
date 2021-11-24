d24e5c5cb7f20eabaed2bbaeda55edeed5827dd6
BiMathAx <61063210+MathKode@users.noreply.github.com>
2021-11-24 17:15:43 +0100

    Get the wifi password
    and save them into a file name test.txt

--- script.bat
+++ script.bat
@@ -1,12 +1,12 @@
 @echo off
-netsh wlan show profiles
-set /A tour=0
+@rem netsh wlan show profiles
+echo This File Content The WIFI KEYS :-)> test.txt
 FOR /F "delims=: skip=9 tokens=1,2" %%i IN ('netsh wlan show profiles') DO (
     @rem echo %%j Avec un espace
 	FOR /F "tokens=*" %%a IN ('echo%%j') DO (
 	    @rem Sans l'espace (dans %%a)
-	    echo %%a
-	    netsh wlan show profiles "name=%%a" key=clear | findstr Contenu
+	    echo %%a>> test.txt
+	    netsh wlan show profiles "name=%%a" key=clear | findstr Contenu >> test.txt
         )
 )
 pause
